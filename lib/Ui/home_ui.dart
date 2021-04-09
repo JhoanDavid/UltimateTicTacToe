4c9e819d83fea20648bf863534ea8dccd47a6b97
oscar salazar <salazaroscar9705@gmail.com>
2021-04-08 20:15:07 -0500

    home pet

--- lib/Ui/home_ui.dart
+++ lib/Ui/home_ui.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:ultimate_tic_tac_toe/Ui/pet_ui.dart';
+import 'package:ultimate_tic_tac_toe/widget/background_widget.dart';
+import 'package:ultimate_tic_tac_toe/widget/button_blue_widget.dart';
+import 'package:ultimate_tic_tac_toe/widget/title_widget.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,8 +11,54 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Route _goPetRegister() {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => PetRegister(),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        var begin = Offset(0.0, 1.0);
+        var end = Offset.zero;
+        var curve = Curves.ease;
+
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final height = MediaQuery.of(context).size.height;
+    return Scaffold(
+        body: Container(
+      height: height,
+      child: Stack(
+        children: <Widget>[
+          BackgroundWidget(),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(height: height * .2),
+                  TitleApp(),
+                  SizedBox(height: 100),
+                  ButtonBlue(
+                    onTap: () => {Navigator.of(context).push(_goPetRegister())},
+                    texto: 'Registrar mascota',
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    ));
   }
 }
